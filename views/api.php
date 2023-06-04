954c82d23a3c3902fa0a7621c997db8ae2e812f4
Aram Hamo <contact@aramhamo.me>
2023-06-04 19:59:41 +0200

    modified:   views/api.php

--- views/api.php
+++ views/api.php
@@ -1,10 +1,12 @@
 <?php
 use AramHamo\Mvc\Models\User;
 use AramHamo\Mvc\Models\Chat;
+use AramHamo\MvcCore\DB;
 header("Content-Type: application/json");
 
 $user = new User;
 $chat = new Chat;
+$db = new DB;
 
 if(isset(getallheaders()["tokan"])){
   $userData = $user->showWhere("tokan",getallheaders()["tokan"]);
@@ -20,13 +22,19 @@ switch($_POST['action']){
     print_r(json_encode(array("return"=>$chat->create())));
   break;
   case "usernameExists":
-  if($_POST["action"] ?? '' == "usernameExists"){
     if(isset($user->showWhere("username",$_POST["username"])[0])){
       print_r(json_encode(array("return"=>true)));
     }else{
       print_r(json_encode(array("return"=>false)));
     }
-  }
+  break;
+  case "getChatLog":
+    $limit = $_POST['limit'];
+    $withUser = $_POST['withUser'];
+    $stmt = "SELECT * from chat where (from_user=? and to_user=?) or (from_user=? and to_user=?) order by id desc limit ?";
+    $chatLog = $db->conn->prepare($stmt);
+    $chatLog->execute([$userData[0]["username"],$withUser,$withUser,$userData[0]["username"],$limit]);
+    print_r(json_encode($chatLog->fetchAll()));
   break;
 }
 }
